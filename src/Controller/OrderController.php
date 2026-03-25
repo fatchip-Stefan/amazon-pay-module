19fdf8ad8ed465402570c309dd2f9c13fdaca57e
Fatchip-Stefan <stefan.mueller@fatchip.de>
2026-03-25 09:48:28 +0100

    styling

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -47,7 +47,7 @@ class OrderController extends OrderController_parent
     public function init()
     {
         $session = Registry::getSession();
-        if (Registry::getRequest()->getRequestParameter('useAmazonNonExpress') === "true"){
+        if (Registry::getRequest()->getRequestParameter('useAmazonNonExpress') === "true") {
             $session->setVariable('paymentid', Constants::PAYMENT_ID);
             $this->addProductToBasket();
         }
@@ -143,8 +143,13 @@ class OrderController extends OrderController_parent
         ) {
             $amazonConfig = oxNew(Config::class);
             if ($amazonConfig->getAmazonPayLogging()) {
-                $logger->log(LogLevel::DEBUG,
-                    \OxidEsales\Eshop\Core\Registry::getLang()->translateString('MESSAGE_PAYMENT_UNAVAILABLE_PAYMENT', 1) . PHP_EOL .
+                $logger->log(
+                    LogLevel::DEBUG,
+                    \OxidEsales\Eshop\Core\Registry::getLang()->translateString(
+                        'MESSAGE_PAYMENT_UNAVAILABLE_PAYMENT',
+                        1
+                    )
+                    . PHP_EOL .
                     'isAmazonpayment: ' . $isAmazonPayment . PHP_EOL .
                     'isAmazonSessionActive: ' . $isAmazonSessionActive . PHP_EOL .
                     'exclude: ' . $exclude . PHP_EOL
@@ -349,7 +354,11 @@ class OrderController extends OrderController_parent
 
         if ($amazonConfig->getAmazonPayLogging()) {
             $logger->log(LogLevel::ERROR,
-                \OxidEsales\Eshop\Core\Registry::getLang()->translateString('MESSAGE_PAYMENT_UNAVAILABLE_PAYMENT', 1) . PHP_EOL .
+                \OxidEsales\Eshop\Core\Registry::getLang()->translateString(
+                    'MESSAGE_PAYMENT_UNAVAILABLE_PAYMENT',
+                    1
+                )
+                . PHP_EOL .
                 'Response: ' . var_dump($result['response']) . PHP_EOL
             );
         }
@@ -434,7 +443,11 @@ class OrderController extends OrderController_parent
                     if ($amazonConfig->getAmazonPayLogging()) {
                         $logger = new Logger();
                         $logger->log(LogLevel::DEBUG,
-                            \OxidEsales\Eshop\Core\Registry::getLang()->translateString('AMAZON_PAY_LASTSHIPSETNOTVALID', 1) . PHP_EOL .
+                            \OxidEsales\Eshop\Core\Registry::getLang()->translateString(
+                                'AMAZON_PAY_LASTSHIPSETNOTVALID',
+                                1
+                            )
+                            . PHP_EOL .
                             'actShipSet: ' . $actShipSet . PHP_EOL .
                             'lastShipSet: ' . $lastShipSet . PHP_EOL
                         );
